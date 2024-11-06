4142cfb02ea03d1976cd6605cef69162a724ff59
hirszu <96964839+pillowified@users.noreply.github.com>
2024-11-06 12:54:02 +0100

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -6,45 +6,59 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <form method="post">
-            <input type="submit" value="Logout" name="submit">
-        </form>
-        <?php
-            session_start();
+        <header>
+            <h2>Kino Database Manager</h2>
+            <form method="get" id="buttons">
+                <input type="submit" name="page" value="Frontpage">
+                <input type="submit" name="page" value="Database">
+                <input type="submit" name="page" value="Settings">
+                <input type="submit" name="page" value="Logout">
+            </form>
+        </header>
+        <div id="main">
+            <?php
+                session_start();
 
-            if (!isset($_SESSION['username'])) {
-                header("Location: login.php");
-                exit();
-            }
-            if ($_SESSION['greeted'] == false) {
-                echo "Welcome, " . $_SESSION['username'] . "!";
-                $_SESSION['greeted'] = true;
-            }
+                if ($_SESSION['greeted'] == false) {
+                    echo "Welcome, " . $_SESSION['username'] . "!";
+                    $_SESSION['greeted'] = true;
+                }
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                if ($_POST["submit"] == "Logout") {
-                    session_destroy();
+                if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                    if ($_GET["page"] == "Logout") {
+                        session_destroy();
+                        header("Location: login.php");
+                        exit();
+                    }
+                }
+
+                // ------------------------------------------- //
+
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
+
+                $host = "localhost";
+                $dbUsername = "root";
+                $dbPassword = "";
+                $dbName = "kino";
+
+                $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+                if ($conn->connect_error) {
+                    die("Connection failed: ".$conn->connect_error);
+                }
+            ?>
+        </div>
+        <footer><h5>Logged in as: <?php 
+
+                if (!isset($_SESSION['username'])) {
                     header("Location: login.php");
                     exit();
+                } else {
+                    echo $_SESSION['username'];
                 }
-            }
-
-            // ------------------------------------------- //
-
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
-
-            $host = "localhost";
-            $dbUsername = "root";
-            $dbPassword = "";
-            $dbName = "kino";
-
-            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
-            if ($conn->connect_error) {
-                die("Connection failed: ".$conn->connect_error);
-            }
-        ?>
+             ?>
+             </h5></footer>
     </body>
 </html>
