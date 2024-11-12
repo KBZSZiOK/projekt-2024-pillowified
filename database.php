274234e6ac6cd8b48e908cf6527f7dd14c861d05
hirszu <96964839+pillowified@users.noreply.github.com>
2024-11-12 13:52:37 +0100

    Add files via upload

--- database.php
+++ database.php
@@ -6,127 +6,59 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php
-            session_start();
-
-            if ($_SERVER["REQUEST_METHOD"] == "GET") {
-                if (isset($_GET["page"])) {
-                    if ($_GET["page"] == "Logout") {
-                        session_destroy();
-                        header("Location: login.php");
-                        exit();
-                    }
-                }
-            }
-
-            // ------------------------------------------- //
-
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
-
-            $host = "localhost";
-            $dbUsername = "root";
-            $dbPassword = "";
-            $dbName = "kino";
-
-            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
-            if ($conn->connect_error) {
-                die("Connection failed: ".$conn->connect_error);
-            }
-
-            $sql = "SELECT * FROM uzytkownicy";
-            $result = $conn->query($sql);
-        
-            if ($result->num_rows > 0) {
-                $user_found = false;
-        
-                while ($user = $result->fetch_assoc()) {
-                    if ($user["ID"] == $_SESSION["user_id"]) {
-                        $user_found = true;
-                    }
-                }
-        
-                if (!$user_found) {
-                    session_destroy();
-                    header("Location: login.php");
-                    exit();
-                }
-            } else {
-                session_destroy();
-                header("Location: login.php");
-                exit();
-            }
-        ?>
         <header>
             <h2>Kino Database Manager</h2>
             <form method="get" id="buttons">
-                <input type="submit" name="page" value="Home">
-                <?php
-                    $host = "localhost";
-                    $dbUsername = "root";
-                    $dbPassword = "";
-                    $dbName = "kino";
-        
-                    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-        
-                    if ($conn->connect_error) {
-                        die("Connection failed: ".$conn->connect_error);
-                    }
-        
-                    $sql = "SELECT * FROM uzytkownicy";
-                    $result = $conn->query($sql);
-                     
-                    while ($user = $result->fetch_assoc()) {
-                        if ($user["ID"] == $_SESSION["user_id"]) {
-                            if ($user["Access"] == "1") {
-                                echo '<input type="submit" name="page" value="Database">';
-                            }
-                        }
-                    }
-                ?>
+                <input type="submit" name="page" value="Frontpage">
+                <input type="submit" name="page" value="Database">
                 <input type="submit" name="page" value="Settings">
                 <input type="submit" name="page" value="Logout">
             </form>
         </header>
         <div id="main">
             <?php
+                session_start();
+
                 if ($_SESSION['greeted'] == false) {
                     echo "Welcome, " . $_SESSION['username'] . "!";
                     $_SESSION['greeted'] = true;
                 }
-                if (isset($_GET["page"])) {
-                    if ($_GET["page"] == "Settings") {
-                        echo "<h1>Settings</h1>
-                        <br>
-                        <form method='get' name='page'>
-                        <ul>
-                            <li><input type='submit' value='Theme' style='border:0; background-color:transparent; color:blue; text-decoration:underline;'></li>
-                        </ul>
-                        </form>";
+
+                if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                    if ($_GET["page"] == "Logout") {
+                        session_destroy();
+                        header("Location: login.php");
+                        exit();
                     }
                 }
-                if (isset($_GET["sub_settings"])) {
-                    if ($_GET["sub_settings"] == "Theme") {
-                        echo "aaa";
-                    } 
-                }
-                if (isset($_GET[""])) {
-                    echo "smth";
+
+                // ------------------------------------------- //
+
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
+
+                $host = "localhost";
+                $dbUsername = "root";
+                $dbPassword = "";
+                $dbName = "kino";
+
+                $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+                if ($conn->connect_error) {
+                    die("Connection failed: ".$conn->connect_error);
                 }
             ?>
         </div>
-        <footer><h5>Logged in as:
-        <?php 
-            if (!isset($_SESSION['username'])) {
-                session_destroy();
-                header("Location: login.php");
-                exit();
-            } else {
-                echo $_SESSION['username'];
-            }
-        ?>
-        </h5></footer>
+        <footer><h5>Logged in as: <?php 
+
+                if (!isset($_SESSION['username'])) {
+                    header("Location: login.php");
+                    exit();
+                } else {
+                    echo $_SESSION['username'];
+                }
+             ?>
+             </h5></footer>
     </body>
 </html>
--- login.php
+++ login.php
@@ -7,25 +7,14 @@
         <link rel="stylesheet" href="style1.css">
     </head>
     <body>
-        <div id="info">
-            <h5>* login info:<br>
-                admin - hotdog32 // 1, 1, 1<br>
-                moderator - kebab123 // 1, 1, 0<br>
-                viewer - password // 0, 0, 0
-            </h5>
-        </div>
             <form action="login.php" method="post">
                 <h2 id="login-text">Login</h2>
                 <div id="stuff">
                     <div id="inputs">
-                        <div id="i1">
-                            <label for="username">Username: </label>
-                            <input type="text" id="username" name="username" required><br>
-                        </div>
-                        <div id="i2">
-                            <label for="password">Password: </label>
-                            <input type="password" id="password" name="password" required><br>
-                        </div>
+                        <label for="username">Username: </label>
+                        <input type="text" id="username" name="username" required><br>
+                        <label for="password">Password: </label>
+                        <input type="password" id="password" name="password" required><br>
                     </div>
                     <input type="submit" value="Login" id="sender">
                 </div>
@@ -49,7 +38,7 @@
                 die("Connection failed: ".$conn->connect_error);
             }
 
-            if (isset($_SESSION["user_id"])) {
+            if (isset($_SESSION["username"])) {
                 header("Location: database.php");
                 exit();
             }
@@ -57,35 +46,22 @@
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $username = $conn->real_escape_string($_POST["username"]);
                 $password = $conn->real_escape_string($_POST["password"]);
-            
-                $sql = "SELECT * FROM uzytkownicy";
+
+                $sql = "SELECT * FROM Administratorzy";
                 $result = $conn->query($sql);
-            
+
                 if ($result->num_rows > 0) {
-                    $user_found = false;
-            
-                    while ($user = $result->fetch_assoc()) {
-                        if ($user["Nazwa"] == $username) {
-                            $user_found = true;
-            
-                            if ($user["Haslo"] == $password) {
-                                $_SESSION["username"] = $user["Nazwa"];
-                                $_SESSION["user_id"] = $user["ID"];
-                                $_SESSION["greeted"] = false;
-                                header("Location: database.php?page=Home");
-                                exit();
-                            } else {
-                                echo "Incorrect password!";
-                                exit();
-                            }
-                        }
-                    }
-            
-                    if (!$user_found) {
-                        echo "User not found!";
+                    $user = $result->fetch_assoc();
+                    if ($user["Haslo"] == $password) {
+                        $_SESSION["username"] = $user["Nazwa"];
+                        $_SESSION["user_id"] = $user["ID"];
+                        header("Location: database.php");
+                        exit();
+                    } else {
+                        echo "Incorrect password!";
                     }
                 } else {
-                    echo "No users in the database!";
+                    echo "User not found!";
                 }
             }
 
